8da074bdf10e807005cb034eda53c85fdf3278e9
Your Name <you@example.com>
2019-01-13 05:33:03 +0600

    Broadcasting, Echo & Mix

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,19 +3,18 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
 
         <title>{{config('app.name')}}</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
         <!-- Styles -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://getbootstrap.com/docs/4.1/examples/blog/blog.css">
+        <link rel="stylesheet" href="{{ asset(mix('css/app.css')) }}">
+        <link rel="stylesheet" href="{{ asset(mix('css/blog.css')) }}">
     </head>
     <body>
 
-    <div class="container">
+    <div class="container" id="app">
         @include('partials.navbar')
 
         {{-- @includeWhen(request()->is('/'), 'partials.jumbotron') --}}
@@ -28,23 +27,20 @@
                   @include('partials.sidebar')
               </div><!-- /.row -->
           </main><!-- /.container -->
+        @include('partials.footer')
     </div>
-    @include('partials.footer')
 
-        <!-- Bootstrap core JavaScript
-            ================================================== -->
-            <!-- Placed at the end of the document so the pages load faster -->
-            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-            <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-            <script src="https://getbootstrap.com/docs/4.1/assets/js/vendor/popper.min.js"></script>
-            <script src="https://getbootstrap.com/docs/4.1/dist/js/bootstrap.min.js"></script>
-            <script src="https://getbootstrap.com/docs/4.1/assets/js/vendor/holder.min.js"></script>
+
+
+
+            {{--<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>--}}
+            <script src="{{ asset(mix('js/app.js')) }}"></script>
             <script>
-              Holder.addTheme('thumb', {
-                bg: '#55595c',
-                fg: '#eceeef',
-                text: 'Thumbnail'
-            });
+              Echo.private('post.created')
+                  .listen('PostCreated', (e) => {
+
+                      $.notify(e.post.title + ' has been publish now');
+                  });
         </script>
     </body>
     </html>
